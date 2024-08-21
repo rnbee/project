f9bb68ded6fec11cba3fc9252706bf4ca3cd65eb
rnbee <sonakarpovic873@gmail.com>
2024-08-21 22:04:22 +1200

    another one

--- app/log_settings.py
+++
@@ -1,8 +0,0 @@
-from loguru import logger
-
-logger.add("file.log", rotation="1 MB")
-logger.add("info.log", format="<green>{time} {level} {message}</green>", level="INFO")
-logger.add("info.log", format="{time} {level} {message}", level="WARNING")
-logger.add("error.log", format="{time} {level} {message}", level="ERROR")
-logger.add("debug.log", format="{time} {level} {message}", level="DEBUG")
-logger.add("info.log", format="{time} {level} {message}", level="CRITICAL")
